f0b283cc4c9e89e6a852188242ee59bb6b299fca
sapscode <saptarshi0009@gmail.com>
2026-03-25 23:22:14 +0530

    build

--- src/App.jsx
+++ src/App.jsx
@@ -14,36 +14,41 @@ import { loader as singleCocktailLoader } from "./pages/Cocktail";
 
 import { action as newsLetterAction } from "./pages/Newsletter";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <HomeLayout />,
+			errorElement: <Error />,
+			children: [
+				{
+					index: true,
+					element: <Landing />,
+					loader: landingLoader,
+					errorElement: <SinglePageError />
+				},
+				{
+					path: "about",
+					element: <About />
+				},
+				{
+					path: "newsletter",
+					element: <Newsletter />,
+					action: newsLetterAction
+				},
+				{
+					path: "cocktail/:id",
+					errorElement: <SinglePageError />,
+					loader: singleCocktailLoader,
+					element: <Cocktail />
+				}
+			]
+		}
+	],
 	{
-		path: "/",
-		element: <HomeLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				index: true,
-				element: <Landing />,
-				loader: landingLoader,
-				errorElement: <SinglePageError />
-			},
-			{
-				path: "about",
-				element: <About />
-			},
-			{
-				path: "newsletter",
-				element: <Newsletter />,
-				action: newsLetterAction
-			},
-			{
-				path: "cocktail/:id",
-				errorElement: <SinglePageError />,
-				loader: singleCocktailLoader,
-				element: <Cocktail />
-			}
-		]
+		basename: "/mix-master/"
 	}
-]);
+);
 const App = () => {
 	return <RouterProvider router={router}></RouterProvider>;
 };
